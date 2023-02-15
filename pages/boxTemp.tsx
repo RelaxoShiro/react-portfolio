f808e58f6ef9936a1079b4239ed8525065efeb75
RelaxoShiro <tolic12380@gmail.com>
2023-02-15 04:36:01 +0100

    v0.7

--- pages/boxTemp.tsx
+++ pages/boxTemp.tsx
@@ -4,11 +4,11 @@ import Image from "next/image"
 const BoxTemp = (props) => {
   return (
     
-<div className="">
-    <Image src={props.img1} alt={""} className= '  rounded-md '/>
+<div className="hover:scale-105 transition-all duration-300 ">
+    <Image src={props.img1} alt={""} className= 'max-w-md  rounded-md border-black border border-dashed hover:animate-pulse'/>
     <div className='flex justify-between'>
-      <h1 className=' text-l font-semibold'>{props.name}</h1>
-      <p className='text-gray-400'>Brading Design</p>
+      <h1 className='text-sm'>{props.name}</h1>
+      <p className='text-gray-400 text-xs'>Branding | Design</p>
     </div>
 </div>
 
